641cd97fbb1642550ab111bd2adbec07fb7a94bd
Nafis Handoko <naffisioner@gmail.com>
2023-04-21 23:44:58 +0700

    add css to remove chrome scrollbar

--- src/components/user/layout.tsx
+++ src/components/user/layout.tsx
@@ -8,7 +8,7 @@ export default function UserLayout({ children }: { children: any }) {
       <UserTopbar />
       <div className='flex flex-row p-4 gap-4 h-full'>
         <UserLeftSidebar />
-        <div className='w-3/5 flex flex-col gap-5 max-h-[90vh] overflow-y-scroll overflow-x-hidden'>
+        <div className='w-3/5 flex flex-col gap-5 max-h-[90vh] overflow-y-scroll overflow-x-hidden no-scrollbar'>
           {children}
         </div>
         <UserRightSidebar />
