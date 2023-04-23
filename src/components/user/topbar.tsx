1fd0a8c606ba1336dbe782591b2169ca0d5f12de
Nafis Handoko <naffisioner@gmail.com>
2023-04-23 17:56:00 +0700

    add SearchDropdown

--- src/components/user/topbar.tsx
+++ src/components/user/topbar.tsx
@@ -24,18 +24,49 @@ function ProfileDropdown({ ...props }) {
 }
 
 function SearchDropdown(props: any) {
+    const dummySearch = [
+        {
+            name: 'Le Minerale',
+            username: 'azer_reza'
+        },
+        {
+            name: 'Le Minerale',
+            username: 'azer_reza'
+        },
+        {
+            name: 'Le Minerale',
+            username: 'azer_reza'
+        },
+    ]
+
     return (
-        <div></div>
+        <div className='bg-white p-6 flex flex-col gap-3 shadow-lg'>
+            {dummySearch && dummySearch.map((search) => (
+                <div className='flex flex-row gap-3 items-center'>
+                    <div className='bg-center bg-cover w-9 aspect-square rounded-full' style={{ backgroundImage: `url(${dummyPhoto})` }}></div>
+                    <div className='flex flex-col'>
+                        <h4 className='font-medium'>{search.name}</h4>
+                        <span className='text-gray-400 text-sm'>@{search.username}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
     )
 }
 
 export default function UserTopbar(props: any) {
-    const [showDropdown, setShowDropdown] = useState(false)
+    const [showProfileDropdown, setShowProfileDropdown] = useState(false)
+    const [showSearchDropdown, setShowSearchDropdown] = useState(false)
+
     const profileDropdownRef = useRef<HTMLDivElement>(null)
+    const searchDropdownRef = useRef<HTMLDivElement>(null)
 
     const closeDropdown = (e: Event) => {
-        if (profileDropdownRef.current && showDropdown && !profileDropdownRef.current.contains(e.target as Node)) {
-            setShowDropdown(false)
+        if (profileDropdownRef.current && showProfileDropdown && !profileDropdownRef.current.contains(e.target as Node)) {
+            setShowProfileDropdown(false)
+        }
+        if (searchDropdownRef.current && showSearchDropdown && !searchDropdownRef.current.contains(e.target as Node)) {
+            setShowSearchDropdown(false)
         }
     }
 
@@ -45,16 +76,21 @@ export default function UserTopbar(props: any) {
 
     return (
         <div className={`bg-white py-2 relative ${props.className}`}>
-            {showDropdown && <div className='absolute right-5 top-14' ref={profileDropdownRef}>
+            {showProfileDropdown && <div className='absolute right-5 top-14' ref={profileDropdownRef}>
                 <ProfileDropdown />
             </div>}
             <div className="container mx-auto flex flex-row justify-between items-center">
                 <img src={Logo} alt="" />
-                <input type="text" placeholder="Pencarian" className='rounded-md bg-relazee-gray px-3 py-1 w-72' />
+                <div className='w-72 relative'>
+                    <input type="text" placeholder="Pencarian" className='rounded-md bg-relazee-gray px-3 py-1 w-full' onFocus={() => setShowSearchDropdown(true)} />
+                    {showSearchDropdown && <div className='absolute top-10 right-0 left-0' ref={searchDropdownRef}>
+                        <SearchDropdown />
+                    </div>}
+                </div>
                 <div className='flex flex-row gap-5 items-center'>
                     <a href="#"><i className="bi bi-bell"></i></a>
                     <a href="#"><i className="bi bi-chat-left-dots"></i></a>
-                    <button onClick={() => setShowDropdown(!showDropdown)} className='flex flex-row items-center gap-1'>
+                    <button onClick={() => setShowProfileDropdown(!showProfileDropdown)} className='flex flex-row items-center gap-1'>
                         <div className="bg-center bg-cover w-7 aspect-square rounded-full" style={{ backgroundImage: `url(${dummyPhoto})` }}></div>
                         <i className="bi bi-caret-down-fill text-xs"></i>
                     </button>
