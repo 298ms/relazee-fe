a36fb0aaf7ad5de3d3c9dc02e62a8d8c37f658bf
Nafis Handoko <naffisioner@gmail.com>
2023-04-24 21:02:43 +0700

    add DetailMenu component in kompetisi-detail

--- src/pages/user/kompetisi-detail.tsx
+++ src/pages/user/kompetisi-detail.tsx
@@ -1,6 +1,7 @@
 import UserLayout from "../../components/user/layout";
 import DummyPoster from '../../assets/dummy-poster.png'
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 export function UserKompetisiDetailInfo() {
     return (
@@ -35,22 +36,56 @@ export function UserKompetisiDetailInfo() {
 export function UserKompetisiDetailDeskripsi() {
     return (
         <p className="text-gray-500 whitespace-pre-wrap">
-            Lorem ipsum dolor sit amet consec te tur. Egestas ligula luctus amet odio sa gittis pharetra sagittis. Orci lacus non tortor et ut nunc venenatis sagittis. Adi piscing proin volutpat tristique cras fri ngilla. Fringilla dis auctor maecenas in teger. 
+            Lorem ipsum dolor sit amet consec te tur. Egestas ligula luctus amet odio sa gittis pharetra sagittis. Orci lacus non tortor et ut nunc venenatis sagittis. Adi piscing proin volutpat tristique cras fri ngilla. Fringilla dis auctor maecenas in teger.
             Consectetur gravida sem integer ut risus aenean eu. Nibh gravida quis maecenas pharetra ornare tortor vive rra. Nibh dignissim pellentesque aliqu am id mauris risus eget. Turpis lectus in semper volutpat mi.
         </p>
+    )
+}
+
+function DetailMenu() {
+    return (
+        <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col gap-3">
+            <button className="flex flex-row items-center gap-3">
+                <i className="bi bi-clipboard text-sm"></i>
+                <span>Salin tautan</span>
+            </button>
+            <button className="flex flex-row items-center gap-3">
+                <i className="bi bi-share-fill text-sm"></i>
+                <span>Bagikan</span>
+            </button>
+            <button className="flex flex-row items-center gap-3">
+                <i className="bi bi-bookmark-plus text-sm"></i>
+                <span>Simpan</span>
+            </button>
+        </div>
     )
 }
 
 export default function UserKompetisiDetail() {
     const navigate = useNavigate()
 
+    const [showDetailMenu, setShowDetailMenu] = useState(false)
+
+    const detailMenuRef = useRef<HTMLDivElement>(null)
+
+    const closeDetailMenu = (e: Event) => {
+        if (detailMenuRef.current && showDetailMenu && !detailMenuRef.current.contains(e.target as Node)) {
+            setShowDetailMenu(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', closeDetailMenu)
+    })
+
     return (
         <UserLayout>
             <div className="bg-white rounded-lg p-6 flex flex-col gap-5">
-                <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row items-center justify-between relative">
                     <button onClick={() => navigate(-1)} className="bg-relazee-black w-7 flex items-center justify-center aspect-square text-white rounded-full"><i className="bi bi-arrow-left-short"></i></button>
                     <h2 className="text-gray-500 font-medium text-lg">National Paper Competition HMDEP USU 2022</h2>
-                    <button><i className="bi bi-three-dots-vertical"></i></button>
+                    <button onClick={() => setShowDetailMenu(!showDetailMenu)}><i className="bi bi-three-dots-vertical"></i></button>
+                    {showDetailMenu && <div className="absolute top-[100%] right-0" ref={detailMenuRef}><DetailMenu /></div>}
                 </div>
                 <div className="flex flex-row items-strecth gap-6">
                     <img src={DummyPoster} alt="" className="w-full rounded-md" />
