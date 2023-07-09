3aa5b23fac481b7b8405fa295e831dae1b8c0ecf
Nafis Handoko <naffisioner@gmail.com>
2023-07-09 14:03:46 +0700

    add tags-input component

---
+++ src/components/shared/tags-input.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function TagsInput(){
+    const [tags, setTags] = useState<string[]>([])
+
+    function handleKeyDown(e: any){
+        if(e.key !== 'Enter') return
+        const value = e.target.value
+        if(!value.trim()) return
+        setTags([...tags, value])
+        e.target.value = ''
+    }
+
+    function removeTag(index: any){
+        setTags(tags.filter((el, i) => i !== index))
+    }
+
+    return (
+        <div className="max-w-full flex flex-row flex-wrap rounded-md bg-transparent border px-4 py-2 gap-2">
+            { tags.map((tag, index) => (
+                <div className="bg-relazee-gray rounded-md px-2" key={index}>
+                    <span className="text">{tag}</span>
+                    <span className="ml-2" onClick={() => removeTag(index)}>&times;</span>
+                </div>
+            )) }
+            <input onKeyDown={handleKeyDown} type="text" className="focus:outline-none w-full" placeholder="Type your skill and press enter to add" />
+        </div>
+    )
+}
+
+export default TagsInput
--- src/pages/mahasiswa/onboarding.tsx
+++ src/pages/mahasiswa/onboarding.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
+import TagsInput from '../../components/shared/tags-input'
 
 export default function MahasiswaOnboarding() {
     return (
@@ -9,8 +11,17 @@ export default function MahasiswaOnboarding() {
                     <img src={Logo} alt="" />
                 </div>
             </div>
-            <div className='flex flex-col h-full'>
-                
+            <div className='flex flex-col h-full container mx-auto mt-5'>
+                <div className='flex justify-end'>
+                    <Link to={'/'} className='inline-block rounded-lg border border-gray-300 px-4 py-1'>Lewati</Link>
+                </div>
+                <div className='flex flex-col items-center w-full'>
+                    <div className='flex flex-col max-w-[500px]'>
+                        <h1 className='font-bold text-3xl text-center'>Portofolio</h1>
+                        <p className='text-center'>Dapatkan manfaat yang luar biasa dari komunitas. Daftarlah sekarang dan rasakan manfaatnya!</p>
+                        <TagsInput />
+                    </div>
+                </div>
             </div>
         </div>
     )
