af27b2004a7f9cc86a0fc970bfe65c28cf7f07b5
Nafis Handoko <naffisioner@gmail.com>
2023-07-09 18:30:54 +0700

    change a to navlink in left-sidebar

--- src/components/mahasiswa/left-sidebar.tsx
+++ src/components/mahasiswa/left-sidebar.tsx
@@ -1,4 +1,6 @@
-export default function MahasiswaLeftSidebar(props:any) {
+import { NavLink } from "react-router-dom"
+
+export default function MahasiswaLeftSidebar(props: any) {
     const dummyPhoto = 'https://source.unsplash.com/random/?person'
 
     return (
@@ -9,26 +11,42 @@ export default function MahasiswaLeftSidebar(props:any) {
                 <a href="#" className="text-gray-500 text-sm">View Profile</a>
             </div>
             <div className="flex flex-col items-start gap-5 px-14">
-                <a href="#" className="flex flex-row items-center gap-3">
-                    <div className="bg-relazee-blue text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center"><i className="bi bi-house-door"></i></div>
-                    <span className="text-relazee-blue">Beranda</span>
-                </a>
-                <a href="#" className="flex flex-row items-center gap-3">
-                    <div className="bg-relazee-blue text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center"><i className="bi bi-person"></i></div>
-                    <span className="text-relazee-blue">Profil</span>
-                </a>
-                <a href="#" className="flex flex-row items-center gap-3">
-                    <div className="bg-relazee-blue text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center"><i className="bi bi-chat-left-dots"></i></div>
-                    <span className="text-relazee-blue">Pesan</span>
-                </a>
-                <a href="#" className="flex flex-row items-center gap-3">
-                    <div className="bg-relazee-blue text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center"><i className="bi bi-bell"></i></div>
-                    <span className="text-relazee-blue">Notifikasi</span>
-                </a>
-                <a href="#" className="flex flex-row items-center gap-3">
-                    <div className="bg-relazee-blue text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center"><i className="bi bi-gear"></i></div>
-                    <span className="text-relazee-blue">Pengaturan</span>
-                </a>
+                <NavLink className="flex flex-row items-center gap-3" to={'/'} children={({ isActive }) => (
+                    <>
+                        <div className={`${isActive ? 'bg-relazee-blue' : 'bg-gray-400'} text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center`}><i className="bi bi-house-door"></i></div>
+                        <span className={`${isActive ? 'text-relazee-blue' : 'text-gray-400'}`}>Beranda</span>
+                    </>
+                )} />
+                <NavLink className="flex flex-row items-center gap-3" to={'/profile'} children={({ isActive }) => (
+                    <>
+                        <div className={`${isActive ? 'bg-relazee-blue' : 'bg-gray-400'} text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center`}><i className="bi bi-person"></i></div>
+                        <span className={`${isActive ? 'text-relazee-blue' : 'text-gray-400'}`}>Profil</span>
+                    </>
+                )} />
+                <NavLink className="flex flex-row items-center gap-3" to={'/chats'} children={({ isActive }) => (
+                    <>
+                        <div className={`${isActive ? 'bg-relazee-blue' : 'bg-gray-400'} text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center`}><i className="bi bi-chat-left-dots"></i></div>
+                        <span className={`${isActive ? 'text-relazee-blue' : 'text-gray-400'}`}>Pesan</span>
+                    </>
+                )} />
+                <NavLink className="flex flex-row items-center gap-3" to={'/projects'} children={({ isActive }) => (
+                    <>
+                        <div className={`${isActive ? 'bg-relazee-blue' : 'bg-gray-400'} text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center`}><i className="bi bi-columns"></i></div>
+                        <span className={`${isActive ? 'text-relazee-blue' : 'text-gray-400'}`}>Project</span>
+                    </>
+                )} />
+                <NavLink className="flex flex-row items-center gap-3" to={'/notifications'} children={({ isActive }) => (
+                    <>
+                        <div className={`${isActive ? 'bg-relazee-blue' : 'bg-gray-400'} text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center`}><i className="bi bi-bell"></i></div>
+                        <span className={`${isActive ? 'text-relazee-blue' : 'text-gray-400'}`}>Notifikasi</span>
+                    </>
+                )} />
+                <NavLink className="flex flex-row items-center gap-3" to={'/settings'} children={({ isActive }) => (
+                    <>
+                        <div className={`${isActive ? 'bg-relazee-blue' : 'bg-gray-400'} text-white text-sm rounded-full w-7 aspect-square flex items-center justify-center`}><i className="bi bi-gear"></i></div>
+                        <span className={`${isActive ? 'text-relazee-blue' : 'text-gray-400'}`}>Pengaturan</span>
+                    </>
+                )} />
             </div>
         </div>
     )
