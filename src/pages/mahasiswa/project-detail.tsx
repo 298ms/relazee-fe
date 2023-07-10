3c8c1729842f7c230d915149fc29a48d4afb7977
Nafis Handoko <naffisioner@gmail.com>
2023-07-10 09:04:33 +0700

    add tasks section

--- src/pages/mahasiswa/project-detail.tsx
+++ src/pages/mahasiswa/project-detail.tsx
@@ -1,6 +1,33 @@
 import MahasiswaLayout from "../../components/mahasiswa/layout";
 
 export default function MahasiswaProjectDetail() {
+    const tasks = [
+        {
+            team: '298 ms',
+            event: 'Gemastik',
+            description: 'Revisi BAB 3 bagian metode',
+            time: '7 hari yang lalu',
+            status: true,
+            color: 'green'
+        },
+        {
+            team: 'Salah Jurusan',
+            event: 'PIMNAS',
+            description: 'Revisi BAB 3 bagian metode',
+            time: '7 hari yang lalu',
+            status: true,
+            color: 'blue'
+        },
+        {
+            team: 'Sebelumnya',
+            event: 'Gemastik',
+            description: 'Revisi BAB 3 bagian metode',
+            time: '7 hari yang lalu',
+            status: false,
+            color: 'red'
+        },
+    ]
+
     return (
         <MahasiswaLayout showRightSidebar={false}>
             <div className="flex flex-col gap-3">
@@ -38,6 +65,43 @@ export default function MahasiswaProjectDetail() {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <div className="flex flex-row items-center justify-between">
+                    <h2 className="font-medium text-xl">Tasks</h2>
+                </div>
+                <table className="table text-center">
+                    <thead>
+                        <tr>
+                            <th className="py-3">Team</th>
+                            <th>Event</th>
+                            <th>Document</th>
+                            <th>Description</th>
+                            <th>Time</th>
+                            <th>Feedback</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {(tasks && tasks.length > 0) && tasks.map((task, i) => (
+                            <tr className={`bg-white rounded-lg border-l-4 border-${task.color}-400`}>
+                                <td className="py-4">{task.team}</td>
+                                <td>{task.event}</td>
+                                <td>
+                                    <a href="#"><i className="bi bi-file-earmark-text text-relazee-blue text-lg"></i></a>
+                                </td>
+                                <td>{task.description}</td>
+                                <td>{task.time}</td>
+                                <td>
+                                    <button className="text-relazee-blue border rounded-md border-relazee-blue px-4 py-1">Lihat</button>
+                                </td>
+                                <td>
+                                    <i className={`bi bi-check-square-fill ${task.status ? 'text-green-500' : 'text-gray-300'}`}></i>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </MahasiswaLayout>
     )
