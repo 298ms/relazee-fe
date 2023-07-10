a4c23e691d2521841f15d21f7c7b0395fa15a21d
Nafis Handoko <naffisioner@gmail.com>
2023-07-11 00:08:11 +0700

    disable input when smartbot random is active

--- src/components/shared/tags-input.tsx
+++ src/components/shared/tags-input.tsx
@@ -1,29 +1,35 @@
 import { useState } from 'react'
 
-function TagsInput(){
+function TagsInput({ ...props }) {
     const [tags, setTags] = useState<string[]>([])
 
-    function handleKeyDown(e: any){
-        if(e.key !== 'Enter') return
+    function handleKeyDown(e: any) {
+        if (e.key !== 'Enter') return
         const value = e.target.value
-        if(!value.trim()) return
+        if (!value.trim()) return
         setTags([...tags, value])
         e.target.value = ''
     }
 
-    function removeTag(index: any){
+    function removeTag(index: any) {
         setTags(tags.filter((el, i) => i !== index))
     }
 
     return (
         <div className="max-w-full flex flex-row flex-wrap rounded-md bg-transparent border px-4 py-2 gap-2">
-            { tags.map((tag, index) => (
+            {tags.map((tag, index) => (
                 <div className="bg-relazee-gray rounded-md px-2" key={index}>
                     <span className="text">{tag}</span>
                     <span className="ml-2 cursor-pointer" onClick={() => removeTag(index)}>&times;</span>
                 </div>
-            )) }
-            <input onKeyDown={handleKeyDown} type="text" className="focus:outline-none w-full" placeholder="Type your skill and press enter to add" />
+            ))}
+            <input
+                onKeyDown={handleKeyDown}
+                type="text"
+                className="focus:outline-none w-full"
+                placeholder="Type your skill and press enter to add"
+                {...props}
+            />
         </div>
     )
 }
--- src/pages/mahasiswa/smartbot.tsx
+++ src/pages/mahasiswa/smartbot.tsx
@@ -107,13 +107,8 @@ export default function MahasiswaSmartbot() {
     )
 }
 
-const ToggleSwitch = () => {
+const ToggleSwitch = ({ isRandom, handleRandom }: any) => {
     // https://tailwind-elements.com/docs/standard/forms/switch/
-    const [isChecked, setIsChecked] = useState(false);
-
-    const handleToggle = () => {
-        setIsChecked(!isChecked);
-    };
 
     return (
         <div className="flex items-center gap-2 mx-auto">
@@ -123,8 +118,8 @@ const ToggleSwitch = () => {
             >Random</label>
             <input
                 type="checkbox"
-                checked={isChecked}
-                onChange={handleToggle}
+                checked={isRandom}
+                onChange={handleRandom}
                 role="switch"
                 id="flexSwitchChecked"
                 className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
@@ -139,6 +134,12 @@ function SearchForm({ setDisplayForm }: any) {
     const angkatanHandleChange = (val: Number) => {
         setAngkatan(val)
     }
+
+    const [isRandom, setIsRandom] = useState(false);
+
+    const handleRandom = () => {
+        setIsRandom(!isRandom);
+    };
 
     return (
         <div className='h-screen overflow-y-hidden'>
@@ -155,15 +156,21 @@ function SearchForm({ setDisplayForm }: any) {
                     <div className='flex flex-col w-[400px] gap-3'>
                         <h1 className='font-bold text-3xl text-center'>Smart Bot</h1>
                         <p className='text-center'>Dapatkan manfaat yang luar biasa dari komunitas</p>
-                        <ToggleSwitch />
+                        <ToggleSwitch isRandom={isRandom} handleRandom={handleRandom} />
                         <div className='flex flex-col gap-1'>
                             <label htmlFor="" className='text-gray-500 font-medium'>Fakultas</label>
-                            <TagsInput />
+                            <TagsInput disabled={isRandom ? true : false} />
                         </div>
                         <div className='flex flex-col gap-1'>
                             <label htmlFor="" className='text-gray-500 font-medium'>Angkatan</label>
                             <div className="relative w-full">
-                                <select id="style" defaultValue={angkatan as number} onChange={(e) => angkatanHandleChange} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+                                <select
+                                    id="style"
+                                    defaultValue={angkatan as number}
+                                    onChange={(e) => angkatanHandleChange}
+                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                                    disabled={isRandom ? true : false}
+                                >
                                     <option value={0}>Pilih angkatan</option>
                                     <option value={1}>2019</option>
                                     <option value={2}>2020</option>
@@ -174,7 +181,7 @@ function SearchForm({ setDisplayForm }: any) {
                         </div>
                         <div className='flex flex-col gap-1'>
                             <label htmlFor="" className='text-gray-500 font-medium'>Skills</label>
-                            <TagsInput />
+                            <TagsInput disabled={isRandom ? true : false} />
                         </div>
                         <button className='bg-relazee-blue rounded-lg py-2 text-white' onClick={() => { setDisplayForm(false) }}>Mulai</button>
                     </div>
