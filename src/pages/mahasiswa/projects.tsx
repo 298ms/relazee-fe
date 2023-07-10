d9f17dd9fc9581891b6f0aebede218d3c77bbb2e
Nafis Handoko <naffisioner@gmail.com>
2023-07-10 08:03:51 +0700

    add projects section in projects page

--- src/pages/mahasiswa/projects.tsx
+++ src/pages/mahasiswa/projects.tsx
@@ -11,20 +11,38 @@ export default function MahasiswaProject() {
             color: 'green'
         },
         {
-            team: '298 ms',
-            event: 'Gemastik',
+            team: 'Salah Jurusan',
+            event: 'PIMNAS',
             description: 'Revisi BAB 3 bagian metode',
             time: '7 hari yang lalu',
             status: true,
-            color: 'green'
+            color: 'blue'
+        },
+        {
+            team: 'Sebelumnya',
+            event: 'Gemastik',
+            description: 'Revisi BAB 3 bagian metode',
+            time: '7 hari yang lalu',
+            status: false,
+            color: 'red'
+        },
+    ]
+
+    const projects = [
+        {
+            team: '298 ms',
+            lomba: 'National Paper Competition HMDEP USU 2022',
+            color: 'blue'
         },
         {
             team: '298 ms',
-            event: 'Gemastik',
-            description: 'Revisi BAB 3 bagian metode',
-            time: '7 hari yang lalu',
-            status: true,
-            color: 'green'
+            lomba: 'National Paper Competition HMDEP USU 2022',
+            color: 'blue'
+        },
+        {
+            team: '298 ms',
+            lomba: 'National Paper Competition HMDEP USU 2022',
+            color: 'blue'
         },
     ]
 
@@ -52,23 +70,43 @@ export default function MahasiswaProject() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white rounded-lg border-l-4 border-green-400">
-                            <td className="py-4">298 ms</td>
-                            <td>Gemastik</td>
-                            <td>
-                                <a href="#"><i className="bi bi-file-earmark-text text-relazee-blue text-lg"></i></a>
-                            </td>
-                            <td>Revisi BAB 3 bagian metode</td>
-                            <td>7 hari yang lalu</td>
-                            <td>
-                                <button className="text-relazee-blue border rounded-md border-relazee-blue px-4 py-1">Lihat</button>
-                            </td>
-                            <td>
-                                <i className="bi bi-check-square-fill text-green-500"></i>
-                            </td>
-                        </tr>
+                        {(tasks && tasks.length > 0) && tasks.map((task, i) => (
+                            <tr className={`bg-white rounded-lg border-l-4 border-${task.color}-400`}>
+                                <td className="py-4">{task.team}</td>
+                                <td>{task.event}</td>
+                                <td>
+                                    <a href="#"><i className="bi bi-file-earmark-text text-relazee-blue text-lg"></i></a>
+                                </td>
+                                <td>{task.description}</td>
+                                <td>{task.time}</td>
+                                <td>
+                                    <button className="text-relazee-blue border rounded-md border-relazee-blue px-4 py-1">Lihat</button>
+                                </td>
+                                <td>
+                                    <i className={`bi bi-check-square-fill ${task.status ? 'text-green-500' : 'text-gray-300'}`}></i>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
+            </div>
+            <div className="flex flex-col gap-3">
+                <div className="flex flex-row items-center justify-between">
+                    <h2 className="font-medium text-xl">Projects</h2>
+                    {/* <a className="text-relazee-blue">Lihat semua <i className="bi bi-chevron-right"></i></a> */}
+                </div>
+                <div className="flex flex-row flex-wrap items-stretch gap-3">
+                    {(projects && projects.length > 0) && projects.map((project, i) => (
+                        <div className="rounded-lg p-4 bg-blue-400 text-white flex flex-col gap-2 w-[180px]">
+                            <h3 className="text-lg font-medium">{project.team}</h3>
+                            <p className="text-sm">{project.lomba}</p>
+                        </div>
+                    ))}
+                    <div className="rounded-lg p-4 bg-white flex flex-col justify-center items-center w-[180px]">
+                        <i className="bi bi-plus-square"></i>
+                        <span>Tambah project</span>
+                    </div>
+                </div>
             </div>
         </MahasiswaLayout>
     )
