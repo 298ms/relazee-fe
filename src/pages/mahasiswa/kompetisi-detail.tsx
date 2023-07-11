3678ce250091aebfe6c9e2c0c005e0c2faac7e72
Nafis Handoko <naffisioner@gmail.com>
2023-07-11 09:35:20 +0700

    change data in kompetisi-detail

--- src/pages/mahasiswa/kompetisi-detail.tsx
+++ src/pages/mahasiswa/kompetisi-detail.tsx
@@ -1,5 +1,5 @@
 import MahasiswaLayout from "../../components/mahasiswa/layout";
-import DummyPoster from '../../assets/dummy-poster.png'
+import GemastikLogo from '../../assets/dummy poster/gemastik.png'
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import KompetisiCard from "../../components/mahasiswa/kompetisi-card";
@@ -11,11 +11,11 @@ export function MahasiswaKompetisiDetailInfo() {
         <div className="flex flex-col gap-4">
             <div className="flex flex-col">
                 <h3 className="text-gray-500 text-xs font-light">Jumlah Tim</h3>
-                <span>3-5 orang</span>
+                <span>3 orang</span>
             </div>
             <div className="flex flex-col">
-                <h3 className="text-gray-500 text-xs font-light">Hadiah</h3>
-                <span>Rp 8.000.000</span>
+                <h3 className="text-gray-500 text-xs font-light">Total Hadiah</h3>
+                <span>Rp 240.000.000</span>
             </div>
             <div className="flex flex-col">
                 <h3 className="text-gray-500 text-xs font-light">Biaya Pendaftaran</h3>
@@ -23,15 +23,22 @@ export function MahasiswaKompetisiDetailInfo() {
             </div>
             <div className="flex flex-col">
                 <h3 className="text-gray-500 text-xs font-light">Periode Pendaftaran</h3>
-                <span>20 Mei 2023 - 12 Juni 2023</span>
+                <span>12 Mei 2023 - 12 Juli 2023</span>
             </div>
             <div className="flex flex-col">
                 <h3 className="text-gray-500 text-xs font-light">Kategori</h3>
-                <span>Karya Tulis Ilmiah</span>
+                <div className="flex flex-row flex-wrap gap-2">
+                    <span className="bg-relazee-gray rounded-lg px-2">Karya Tulis Ilmiah</span>
+                    <span className="bg-relazee-gray rounded-lg px-2">Programming</span>
+                    <span className="bg-relazee-gray rounded-lg px-2">Cyber Security</span>
+                    <span className="bg-relazee-gray rounded-lg px-2">Bisnis TIK</span>
+                    <span className="bg-relazee-gray rounded-lg px-2">Kota Cerdas</span>
+                    <span className="bg-relazee-gray rounded-lg px-2">Animasi</span>
+                </div>
             </div>
             <div className="flex flex-col">
                 <h3 className="text-gray-500 text-xs font-light">Link Pendaftaran</h3>
-                <a href="#">unej.id/Lomba-test</a>
+                <a className="text-relazee-blue" target="_blank" href="https://gemastik.kemdikbud.go.id/">https://gemastik.kemdikbud.go.id/</a>
             </div>
         </div>
     )
@@ -39,8 +46,7 @@ export function MahasiswaKompetisiDetailInfo() {
 export function MahasiswaKompetisiDetailDeskripsi() {
     return (
         <p className="text-gray-500 whitespace-pre-wrap">
-            Lorem ipsum dolor sit amet consec te tur. Egestas ligula luctus amet odio sa gittis pharetra sagittis. Orci lacus non tortor et ut nunc venenatis sagittis. Adi piscing proin volutpat tristique cras fri ngilla. Fringilla dis auctor maecenas in teger.
-            Consectetur gravida sem integer ut risus aenean eu. Nibh gravida quis maecenas pharetra ornare tortor vive rra. Nibh dignissim pellentesque aliqu am id mauris risus eget. Turpis lectus in semper volutpat mi.
+            GEMASTIK merupakan program Balai Pengembangan Talenta Indonesia, Pusat Prestasi Nasional, Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi. Program ini ditujukan untuk meningkatkan kompetensi mahasiswa Indonesia, sehingga mampu mengambil peran sebagai agen perubahan dalam memajukan TIK dan pemanfaatannya, baik ketika masih dalam masa studi maupun kelak sesudah lulus studi. Pada tahun ini GEMASTIK digelar untuk ke-16 kalinya dengan tema “TIK untuk Pembangunan Indonesia Maju dan Berkelanjutan”.
         </p>
     )
 }
@@ -86,12 +92,12 @@ export default function MahasiswaKompetisiDetail() {
             <div className="bg-white rounded-lg p-6 flex flex-col gap-5">
                 <div className="flex flex-row items-center justify-between relative">
                     <button onClick={() => navigate(-1)} className="bg-relazee-black w-7 flex items-center justify-center aspect-square text-white rounded-full"><i className="bi bi-arrow-left-short"></i></button>
-                    <h2 className="text-gray-500 font-medium text-lg">National Paper Competition HMDEP USU 2022</h2>
+                    <h2 className="text-gray-500 font-medium text-lg px-7 text-center">Pagelaran Mahasiswa Nasional Bidang Teknologi Informasi dan Komunikasi 2023</h2>
                     <button onClick={() => setShowDetailMenu(!showDetailMenu)}><i className="bi bi-three-dots-vertical"></i></button>
                     {showDetailMenu && <div className="absolute top-[100%] right-0" ref={detailMenuRef}><DetailMenu /></div>}
                 </div>
                 <div className="flex flex-row items-strecth gap-6">
-                    <img src={DummyPoster} alt="" className="w-1/2 rounded-md" />
+                    <img src={GemastikLogo} alt="" className="w-1/2 object-contain rounded-md" />
                     <div className="flex flex-col gap-5 w-1/2">
                         <div className="flex flex-row text-center">
                             <NavLink replace to="info" className={({ isActive, isPending }) => `w-full py-2 ${isActive ? "border-b-2 border-relazee-blue" : "border-b border-relazee-gray"}`
@@ -105,7 +111,7 @@ export default function MahasiswaKompetisiDetail() {
                     </div>
                 </div>
                 <div className="flex flex-row items-stretch gap-2">
-                    <a href="#" className="bg-relazee-blue rounded-lg p-3 text-white w-full text-center">Daftar Sekarang</a>
+                    <a href="https://gemastik.kemdikbud.go.id/" target="_blank" className="bg-relazee-blue rounded-lg p-3 text-white w-full text-center">Daftar Sekarang</a>
                     <a href="#" className="bg-relazee-blue-light rounded-lg text-gray-500 p-3 w-full text-center">Buat Tim</a>
                 </div>
             </div>
